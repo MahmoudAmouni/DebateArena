b1b8e79d932273f0029bcf505bbdf19b7df13438
Mahmoud Amouni <mhmdamoun0@gmail.com>
2026-05-04 17:17:10 +0300

    Server -> updated users routes

--- debatearena-server/src/modules/users/users.routes.ts
+++ debatearena-server/src/modules/users/users.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
 import { authenticate } from '../../middleware/authenticate';
 
 const router = Router();
 const usersRepository = new UsersRepository();
-const usersController = new UsersController(usersRepository);
+const usersService = new UsersService(usersRepository);
+const usersController = new UsersController(usersService);
 
 router.get('/me', authenticate, usersController.getMe);
 
